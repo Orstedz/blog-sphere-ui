cb9b95304674e6f93f9853ef9eeca8af47815e0b
Minh Pham <hypurrliquid@outlook.com>
2025-11-15 10:42:48 +0700

    Refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Title } from '@angular/platform-browser';
   template: '<router-outlet></router-outlet>',
 })
 export class AppComponent implements OnInit {
-  title = 'Tedu Blog Admin UI';
+  title = 'Blog Sphere Admin UI';
 
   constructor(
     private router: Router,
